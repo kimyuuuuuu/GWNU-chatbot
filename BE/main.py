f94da675d93a244f084afeda945726191b69f4a6
hwstar-1204 <hwstar1204@gmail.com>
2024-11-14 00:36:49 +0900

    feat :  FastAPI 서버에 채팅봇 API 추가
    어떤 방식으로 사용할지 결정되지 않아서
    동기, 비동기 호출 두가지 경우 모두 만들어둠

--- BE/main.py
+++ BE/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI, Query
 from dotenv import load_dotenv
+from fastapi import FastAPI, Query
 
+from chatbot import process_question
 
 load_dotenv()
 app = FastAPI()
 
+
+@app.get("/")
+def read_root():
+    return {"hello": "this is fastapi chatbot server \n have a good dev!"}
+
+
+@app.get("/chat/sync")
+def chat_sync(question: str = Query(None, min_length=3, max_length=100)):
+    response, retrieve_docs = process_question(is_sync=True, user_question=question)
+    return {"response": response}
+
+
+@app.get("/chat/async")
+def chat_async(question: str = Query(None, min_length=3, max_length=100)):
+    response, retrieve_docs = process_question(is_sync=False, user_question=question)
+    return {"response": response}
