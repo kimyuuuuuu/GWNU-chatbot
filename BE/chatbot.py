2d722f876d6bb97c70198c9fa80e3fc3df37324b
hwstar-1204 <hwstar1204@gmail.com>
2024-11-14 00:50:41 +0900

    feat : 스트리밍 채팅 기능
    동기/비동기 방식 모두 구현해놓음

--- BE/chatbot.py
+++ BE/chatbot.py
@@ -38,6 +38,14 @@ def get_result_async(chain: Runnable, question: str, context: List[Document]):
     return chain.ainvoke({"question": question, "context": context})
 
 
+def get_result_streaming_sync(chain: Runnable, question: str, context: List[Document]):
+    return chain.stream({"question": question, "context": context})
+
+
+def get_result_streaming_async(chain: Runnable, question: str, context: List[Document]):
+    return chain.astream({"question": question, "context": context})
+
+
 def get_rag_chain(template: str):
     custom_rag_prompt = PromptTemplate.from_template(template)
     model = ChatOpenAI(model="gpt-4o-mini")
@@ -69,5 +77,24 @@ def process_question(is_sync: bool, user_question: str):
         response = get_result_sync(chain, user_question, retrieve_docs)
     else:
         response = get_result_async(chain, user_question, retrieve_docs)
+
+    return response, retrieve_docs
+
+
+def process_question_streaming(is_sync: bool, user_question: str):
+    embeddings = get_embeddings()
+
+    new_db = get_db(embeddings)
+
+    retriever = get_retriever(new_db, 3)
+
+    retrieve_docs = retrieve_docs_sync(retriever, user_question)
+
+    chain = get_rag_chain(init_template)
+
+    if is_sync:
+        response = get_result_streaming_sync(chain, user_question, retrieve_docs)
+    else:
+        response = get_result_streaming_async(chain, user_question, retrieve_docs)
 
     return response, retrieve_docs
--- BE/main.py
+++ BE/main.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI, Query
+from fastapi.responses import StreamingResponse
 
-from chatbot import process_question
+from chatbot import process_question, process_question_streaming
 
 load_dotenv()
 app = FastAPI()
@@ -22,3 +23,37 @@ def chat_sync(question: str = Query(None, min_length=3, max_length=100)):
 def chat_async(question: str = Query(None, min_length=3, max_length=100)):
     response, retrieve_docs = process_question(is_sync=False, user_question=question)
     return {"response": response}
+
+
+@app.get("/chat/streaming_sync")
+def chat_streaming_sync(question: str = Query(None, min_length=3, max_length=100)):
+    def event_stream():
+        try:
+            response, retrieve_docs = process_question_streaming(
+                is_sync=True, user_question=question
+            )
+
+            for chunk in response:
+                if len(chunk) > 0:
+                    yield f"data: {chunk} "
+        except Exception as e:
+            yield f"error: {str(e)}"
+
+    return StreamingResponse(event_stream(), media_type="text/event-stream")
+
+
+@app.get("/chat/streaming_async")
+def chat_streaming_async(question: str = Query(None, min_length=3, max_length=100)):
+    def event_stream():
+        try:
+            response, retrieve_docs = process_question_streaming(
+                is_sync=False, user_question=question
+            )
+
+            for chunk in response:
+                if len(chunk) > 0:
+                    yield f"data: {chunk} "
+        except Exception as e:
+            yield f"error: {str(e)}"
+
+    return StreamingResponse(event_stream(), media_type="text/event-stream")
